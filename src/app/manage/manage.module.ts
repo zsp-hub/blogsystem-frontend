bce723662f31c101b452939ee7e35371ec9c0cfe
vz04 <Victor.S.Zhu@newegg.com>
2019-08-13 14:00:40 +0800

    项目创建

---
+++ src/app/manage/manage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogManageComponent } from './blog-manage/blog-manage.component';
+
+
+
+@NgModule({
+  declarations: [BlogManageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ManageModule { }
